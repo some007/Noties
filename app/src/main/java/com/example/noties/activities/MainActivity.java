85bb5c6999db14ecb819913eb2d63914c423e960
developervws004 <developervws#4>
2021-11-24 01:07:17 +0530

    Notes is saved or not, Showing in Log

--- app/src/main/java/com/example/noties/activities/MainActivity.java
+++ app/src/main/java/com/example/noties/activities/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.noties.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.example.noties.R;
+import com.example.noties.database.NoteDatabase;
+import com.example.noties.entities.Note;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
      public static final int REQUEST_CODE_ADD_NOTE = 1;
@@ -26,5 +32,25 @@ public class MainActivity extends AppCompatActivity {
                 );
             }
         });
+        getNotes();
+    }
+    private void getNotes()
+    {
+        class GetNotesTask extends AsyncTask<Void,Void,List<Note>>
+        {
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NoteDatabase.getDatabase(getApplicationContext())
+                        .noteDao().getAllNotes();
+
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                Log.d("MY_NOTES",notes.toString());
+            }
+        }
+        new GetNotesTask().execute();
     }
 }
